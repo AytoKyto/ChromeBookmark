53793168d27228fb856e3e034e99de657cc0249a
Ayto <mathis.fleury@vaicesi.fr>
2021-12-23 23:15:56 +0100

    fg

--- popup.js
+++ popup.js
@@ -1,17 +1,20 @@
-//window.console = chrome.extension.getBackgroundPage().console;
+var bookmarkTreeNodes = chrome.bookmarks.getTree(
+  function (bookmarkTreeNodes) {
+    //console.log(bookmarkTreeNodes);
+    //console.log(bookmarkTreeNodes[0].children[0].children);
+    for (var i = 0; i < bookmarkTreeNodes.length; i++) {
+      //console.log(bookmarkTreeNodes[i].children[i].children[i].title);
+      //console.log(bookmarkTreeNodes[i].children[i].children[i].url);
 
-const init = () => {
-  console.log('Init popup.js');
 
-};
-function addFunction() {
-  chrome.bookmarks.create({
-    'parentId': extensionsFolderId,
-    'title': 'Extensions doc',
-    'url': 'https://developer.chrome.com/docs/extensions',
+      for (var element of bookmarkTreeNodes[i].children[i].children) {
+        console.log(element);
+        document.getElementById("bookmark_list").innerHTML += "<li><a href=" + element.url + " >" + element.title + "</a><iframe frameborder="+0+"  src=" + element.url + "></iframe></li>";
+        document.querySelectorAll("#bookmark_list li a").forEach(function (a) {
+          a.setAttribute('target', '_blank');
+        })
+      }
+
+
+    }
   });
-  console.log('Add bookmark');
-}
-
-window.onload = init;
-document.getElementById("add").addEventListener("click", addFunction);
