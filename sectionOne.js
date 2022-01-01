f686ace595bdb3b8b53ce7e23841b738d8250d82
Ayto <mathis.fleury@vaicesi.fr>
2022-01-02 00:25:43 +0100

    j

--- sectionOne.js
+++ sectionOne.js
@@ -1,21 +1,25 @@
 var bookmarkTreeNodes = chrome.bookmarks.getTree(
     function (bookmarkTreeNodes) {
-      //console.log(bookmarkTreeNodes);
-      //console.log(bookmarkTreeNodes[0].children[0].children);
-      for (var i = 0; i < bookmarkTreeNodes.length; i++) {
-        //console.log(bookmarkTreeNodes[i].children[i].children[i].title);
-        //console.log(bookmarkTreeNodes[i].children[i].children[i].url);
-  
-  
-        for (var element of bookmarkTreeNodes[i].children[i].children) {
-          console.log(element);
-          document.getElementById("bookmark_list").innerHTML += "<li><a href=" + element.url + " >" + element.title + "</a></li>";
-          document.querySelectorAll("#bookmark_list li a").forEach(function (a) {
-            a.setAttribute('target', '_blank');
-          })
+        var imgUrl = "https://s2.googleusercontent.com/s2/favicons?domain=";
+        //console.log(bookmarkTreeNodes);
+        //console.log(bookmarkTreeNodes[0].children[0].children);
+        for (var i = 0; i < bookmarkTreeNodes.length; i++) {
+            //console.log(bookmarkTreeNodes[i].children[i].children[i].title);
+            //console.log(bookmarkTreeNodes[i].children[i].children[i].url);
+
+
+            for (var element of bookmarkTreeNodes[i].children[i].children) {
+                console.log(element);
+                document.getElementById("bookmark_list").innerHTML += "<li><img src=" + imgUrl + element.url + "><a href=" + element.url + " >" + element.title + "</a></li>";
+                //   document.querySelectorAll("#bookmark_list li a").forEach(function (a) {
+                //     a.setAttribute('target', '_blank');
+                //   })
+            }
+
+
         }
-  
-  
-      }
     });
-  
+
+
+
+
